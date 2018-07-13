f956dcb84f2674af90cd7a80e61eb5e84bc366d3
jxy367 <jxy367@case.edu>
2018-07-12 23:35:14 -0700

    Clean up

--- YodaBot.py
+++ YodaBot.py
@@ -101,7 +101,7 @@ async def cooldown():
 @client.event
 async def on_message(message):
     global on_cooldown
-    guild_id = message.channel.server.id
+    guild_id = message.guild
     if guild_id not in on_cooldown:
         on_cooldown[guild_id] = 0
 
